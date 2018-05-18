e7e45ebd4e2e01a641bd3a65503020683bb468bb
Camilo Vilela <camilo.vilela@dominos.co.uk>
2018-05-18 15:29:58 +0100

    Im sorry

---
+++ ElasticErrorRates.Core/Criteria/Log/FindCriteria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElasticErrorRates.Core.Criteria.Log
+{
+    public class FindCriteria
+    {
+        public string ColumnField { get; set; }
+        public string HttpUrl { get; set; }
+        public string Term { get; set; }
+    }
+}
---
+++ ElasticErrorRates.Core/Criteria/Log/SearchCriteria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElasticErrorRates.Core.Criteria.Log
+{
+    public class SearchCriteria
+    {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public string HttpUrl { get; set; }
+    }
+}
